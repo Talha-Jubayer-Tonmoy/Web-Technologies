4b7ebc4639749da98d3b7dcb2ae965a6a7cf3ff6
Talha-Jubayer-Tonmoy <72748839+Talha-Jubayer-Tonmoy@users.noreply.github.com>
2020-11-02 14:50:26 +0600

    Lab Task_4 uploaded

---
+++ Lab Task 4/Footer.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        .footer
+        {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: purple;
+            text-align: center;
+        }
+    </style>
+</head>
+    <bodY>
+        <footer>
+            <div class="footer">
+                <h5>Copyright @ 2015</h5>
+            </div>
+        </footer>
+    </bodY>
+</html>
---
+++ Lab Task 4/Home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <style>
+        body
+        {
+            background: lightgray;
+            width: 100%;
+            text-align: justify;
+        }
+        div.about
+        {
+            width: 710px;
+            display: inline-block;
+            text-align: right;
+            margin: 20px;
+            
+        }
+        div.information
+        {
+            font-family: Calibri;
+            width: 1250px;
+            text-align: right;
+        }
+    </style>
+    <body>
+    <?php include('Header.php');?>
+        <h1 style="font-size:50px;text-align: center;">
+                <pre>JoJo Fandom</pre>
+        </h1>
+        <hr class="line">
+        <div class="about">
+        <p style="font-size: 20px; translate: 7%;">Welcome to the JoJo Fandom! </p>
+        </div>
+        <div class="information">
+            <h4 style="translate: 7%">Sign up and explore JoJo's unique and bizarre universe!See infos about your favorite characters, villains and ofcourse STANDS. Also please share our page with you friends please :) </a>.</h4>
+        </div>
+    <?php include('Footer.php');?>
+    </body>
+</html>
